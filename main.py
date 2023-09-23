74a80535d1296480ddb2f5841eae9e11c92d5672
kostas-python <diamantopoulos.kostas@hotmail.com>
2023-09-23 21:26:47 +0100

    Day 33: Completed. Added backend,images,temperature,sky

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
-st.title("Weather Forecast For The Next Days")
+# Add title, text input, slider, selectbox, and subheader
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of forecast days")
-
-option = st.selectbox("Select data to view", ("temperature", "sky"))
+                 help="Select the number of forecasted days")
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["25-10-2023", "26-20-2023", "27-10-2023"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create a temperature plot
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
-
+    if option == "Sky":
+        images = {"Clear": "images1/clear.png", "Clouds": "images1/cloud.png",
+                  "Rain": "images1/rain.png", "Snow": "images1/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        print(sky_conditions)
+        st.image(image_paths, width=115)
